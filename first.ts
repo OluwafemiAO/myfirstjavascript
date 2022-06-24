55c20bfc1b675cac933a4d3a3e7d1222e4c2a42e
OluwafemiAO <oluwafemiooyebode@gmail.com>
2022-06-24 16:28:25 +0100

    first typescript

---
+++ first.ts
@@ -0,0 +1,130 @@
+// hello techhub
+/*this is a multiline comment*/
+const Name="My name is Oluwafemi";
+console.log(Name)
+const Place="Akure";
+
+console.log(Place)
+const Church="Catholic";
+
+console.log(Church)
+const State="Ekiti State";
+
+console.log(State)
+const School="I attended AAUA university from 2014-2018";
+
+console.log(School)
+const Workplace="Akure Tech Hub";
+console.log(Workplace)
+const Boss="CTO Jonathan";
+console.log(Boss)
+
+const Career="Blockchain Dev";
+console.log(Career)
+
+const Hubpm="Daniel Pichai Williams";
+console.log(Hubpm);
+
+const Flutter_guy ="Uncle Beam";
+console.log(Flutter_guy);
+
+const Flutter_guy2 ="Dolapo";
+console.log(Flutter_guy2);
+
+const PM ="Mr Olamide Adebayo";
+console.log(PM);
+
+const FCT_money ="Mr Feranmi Jaden"
+console.log(FCT_money)
+
+const Hobbies ="Coding,making money, watching football, reading and enjoying life";
+console.log(Hobbies);
+
+const Books ="The Mob rules";
+console.log(Books);
+
+const Series = "Game of Thrones, Prison Break, Ozark and Sex Education ";
+console.log(Series);
+
+const Phone_number ="08132710891";
+console.log(Phone_number);
+
+const Number_of_hub_subscribers = "We have 25 subscribers";
+console.log(Number_of_hub_subscribers);
+
+const Number_of_shuttles_buses = "Futa has over 300shuttle buses";
+console.log(Number_of_shuttles_buses);
+
+const Number_of_air_conditioners_in_the_hub = "The hub has 5 Air conditioners";
+console.log(Number_of_air_conditioners_in_the_hub);
+
+const Apple_founder = "Steve Jobs";
+console.log(Apple_founder);
+
+const Apple_CEO = "Tim Cook";
+console.log(Apple_CEO);
+
+const Apple_CEO_DOB = "1 November 1960 (age 61 years)";
+console.log(Apple_CEO_DOB);
+
+const Best_male_artistes = "Kanye Omari West, Kendrick Lamar, Tope Alabi, King Sunny Ade and Sufjan Stevens";
+console.log(Best_male_artistes);
+
+const Best_football_player = "Cristiano Ronaldo";
+console.log(Best_football_player);
+
+const Favorite_designers = "Louis Vuitton,Christian Louboutin, Salvatore Ferragamo, Gucci and Guiseppe Zanotti";
+console.log(Favorite_designers);
+
+const Best_club_in_the_world = "Real Madrid";
+console.log(Best_club_in_the_world);
+
+const Real_Madrid_President = "Florentino Perez";
+console.log(Real_Madrid_President);
+
+const Greatest_midfielder_in_the_world = "Zinedine Yazid Zidane";
+console.log(Greatest_midfielder_in_the_world);
+
+const shoe_size = "i wear a Uk size 11 and a EUR size 45.5";
+console.log(shoe_size);
+
+const Best_sitcoms = "How i met your Mother, The Office and Bob hearts Abishola";
+console.log(Best_sitcoms);
+
+const My_discipline_in_university = "I studied Political Science for my first degree";
+console.log(My_discipline_in_university);
+
+const Cars = "Mercedes Benz, BMW, ROlls-Royce, Audi, Ford and Ferrari";
+console.log(Cars);
+
+const Favorite_sport_brands = "Nike, Adidas and Puma";
+console.log(Favorite_sport_brands);
+
+const RAII = "Resource Acquisition is Initialization(RAII). An object memory allocation is tied to its lifetime which is from construction till destruction. Introduced in C++ and also used in ADA and RUST  "
+console.log(RAII);
+
+const Best_Animation = "Family guy, Boondocks, Attack on the Titans, and Avatar the last airbender";
+console.log(Best_Animation);
+
+const Best_basketballer = "Michael Jordan";
+console.log(Best_basketballer);
+
+const Phones = "iPhone, Samsung, Google one plus+, Infinix and many more.";
+console.log(Phones)
+
+const Laptops = "Apple, Hp (Helwett Packard), Dell, Lenovo and so many more.";
+console.log(Laptops);
+
+const Countries = "Germany, Norway, Denmark, Switzerland, Belgium, Sweden, United kingdom, France and Finland.";
+console.log(Countries);
+
+const Nigeria_population = "Over 200million people live in Nigeria, making it the most populated black country in the world."
+console.log(Nigeria_population);
+
+const world_largest_population = "China has the world's largest population with over 1.4billion people.";
+console.log(world_largest_population);
+
+const phone_OS = "Android and ios";
+console.log(phone_OS);
+
+
