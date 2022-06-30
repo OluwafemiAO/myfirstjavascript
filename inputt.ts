bcccaf8cdcb47ab558c0d1b601a727f6c26028e4
OluwafemiAO <oluwafemiooyebode@gmail.com>
2022-06-30 10:05:31 +0100

    creates typescript profile

---
+++ inputt.ts
@@ -0,0 +1,17 @@
+let a = 3;
+let b = 2;
+let c = 6;
+let d = 8;
+let e = 4;
+const calculation1 = (((c*a)-d)/b) + e;
+
+console.log(calculation1);
+
+let f = 2;
+let g = 5;
+let h = 8;
+let i = 11;
+let j = 21;
+let k = 9;
+const calculation = ((h-g) - ((j-i)-k)) /f;
+console.log(calculation);
